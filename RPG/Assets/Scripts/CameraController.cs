23b6a2e586f0e2a71fc3772fa69197cd7c5ad279
NgachaIan <ngacha.duncan@students.jkuat.ac.ke>
2018-09-26 06:54:46 +0300

    A simple click to go game

---
+++ RPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
+
+    public float yawSpeed = 100f;
+
+    private float currentZoom = 10f;
+    private float currentYaw = 0f;
+
+    private void Update()
+    {
+        //Allowing zoom using mouse wheel
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        //Limiting the zoom to specified values
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        //Enable camera rotation
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+
+
+    }
+
+    private void LateUpdate()
+    {
+        //Getting  the camera to follow the player
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+
+}
---
+++ RPG/Assets/Scripts/Interactable.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    //How close the player should be before interacting with an object
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+    }
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
